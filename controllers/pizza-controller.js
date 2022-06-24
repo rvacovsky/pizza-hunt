233611a2a4002b3224631d52c040cd720327f0de
Roger Vacovsky <rvacovsky@gmail.com>
2022-06-24 19:16:05 -0400

    created api routes

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -2,7 +2,7 @@ const { Pizza } = require('../models');
 
 const pizzaController = {
     //get all Pizzas
-    getAllPizzas(req, res) {
+    getAllPizza(req, res) {
         Pizza.find({})
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
